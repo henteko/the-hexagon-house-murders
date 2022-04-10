6f26b08aca8a36c2a53ebf20881903a6653f9b5f
henteko <henteko07@gmail.com>
2022-04-10 07:09:59 +0000

    一通りページを作成した

---
+++ assets/javascript/ending.js
@@ -0,0 +1,11 @@
+document.querySelector("#open-ending-1").addEventListener("click", () => {
+  document.querySelector("#ending-1").style = "display: initial;"
+});
+
+document.querySelector("#open-ending-2").addEventListener("click", () => {
+  document.querySelector("#ending-2").style = "display: initial;"
+});
+
+document.querySelector("#open-ending-3").addEventListener("click", () => {
+  document.querySelector("#ending-3").style = "display: initial;"
+});
